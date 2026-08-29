1ab5cd37d44460bc9f1be11154566c06391f68b0
Shikha <shikha92366@gmail.com>
2026-08-29 18:34:50 +0530

    Time: 1 ms (2.57%) | Memory: 66.4 MB (10.68%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,14 +1,24 @@
+#include<vector>
+using namespace std;
+
 class Solution {
 public:
+  
+    int find_pivot(vector<int> v) {
+	int s = 0, e = v.size() - 1;
+	int mid=(s + e) / 2;
+	while (s < e)
+	{
+		if(v[mid] < v[mid+1])
+     			s=mid+1;
+    		else
+      			e = mid;
+    
+		mid = (s + e) / 2;
+	}
+	return s;
+    }
     int peakIndexInMountainArray(vector<int>& arr) {
-        int ans = 0;
-
-        for(int i = 0; i < arr.size() - 1; i++) {
-            if(arr[i] < arr[i + 1]) {
-                ans = i + 1;
-            }
-        }
-
-        return ans;
+        return find_pivot(arr);
     }
 };
